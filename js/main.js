5f717ca9360ad974677165b96cf11ad941fb2983
Suarwill <23763487k@gmail.com>
2024-02-25 19:32:04 -0300

    actualizacion

--- js/main.js
+++ js/main.js
@@ -1,19 +1,23 @@
 function calcularSalario () {
-    var sueldo = document.getElementById("sueldoBase").value;
+    var sueldoBruto = document.getElementById("sueldoBase").value;
     var gratificacion = document.getElementById("gratificacion").value;
-    if (!gratificacion.checked) {
-        gratificacion.value = "no";
-    }
     var bonoMov = document.getElementById("bonoMovilizacion").value;
     var bonoCol = document.getElementById("bonoColacion").value;
     var afp = document.getElementById("afp").value;
     var prevision = document.getElementById("prevision").value;
     var contrato = document.getElementById("tipocontrato").value;
-    console.log(sueldo)
+
+    var sueldoLiquido = sueldoBruto
+    var noImponibles = parseFloat(bonoMov) + parseFloat(bonoCol)
+
+    console.log(sueldoBruto)
     console.log(gratificacion)
     console.log(bonoMov)
     console.log(bonoCol)
     console.log(afp)
     console.log(prevision)
     console.log(contrato)
+
+    console.log(sueldoLiquido)
+    console.log(noImponibles)
 }
