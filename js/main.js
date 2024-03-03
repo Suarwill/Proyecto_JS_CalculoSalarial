ef1aa3e9c41906ef53f373c8ca4cc70c3b7e8197
Suarwill <23763487k@gmail.com>
2024-03-02 23:30:44 -0300

    Actualizado Back

--- js/main.js
+++ js/main.js
@@ -1,20 +1,35 @@
+function mostrarResultado() {
+    var resultadosSection = document.getElementById("resultados")
+    resultadosSection.style.display = resultadosSection.style.display === "none" ? "block" : "none"
+    
+    var resultados = calcularSalario()
+    document.getElementById("resultadoSueldo").textContent = "Siendo tu sueldo imponible de $" + resultados.imponible
+    document.getElementById("resultadoDescuentos").textContent = "Descuentos Legales: $" + resultados.descuentos
+    document.getElementById("resultadoLiquido").textContent = "Tu Sueldo líquido es de $" + resultados.liquido
+}
 function calcularSalario () {
     var sueldoBruto = parseInt(document.getElementById("sueldoBase").value)
     var gratificacion = document.getElementById("gratificacion").value
+    var montoGratificacion = 0
     if (gratificacion != "no") {
         montoGratificacion = calcularGratificacion(sueldoBruto)
     }
     var bonoMov = parseInt(document.getElementById("bonoMovilizacion").value)
     var bonoCol = parseInt(document.getElementById("bonoColacion").value)
     var afp = parseFloat(document.getElementById("afp").value).toFixed(2) / 100
+        afp = (afp+10)/100
     var prevision = document.getElementById("prevision").value
     var contrato = document.getElementById("tipocontrato").value
 
     var sueldoImponible = sueldoBruto + montoGratificacion + bonoMov + bonoCol
-    var descuentosLegales = (sueldoImponible * ((afp+10)/100)) + (sueldoImponible * 0.07) 
-    var sueldoLiquido = sueldoImponible - descuentosLegales
+    var descuentosLegales = (sueldoImponible * afp + sueldoImponible * 0.07 )
+    var sueldoLiquido = parseInt(sueldoImponible - descuentosLegales)
 
-    console.log("Su Sueldo es de $ " + sueldoLiquido)
+    return {
+        imponible: sueldoImponible,
+        descuentos: descuentosLegales,
+        liquido: sueldoLiquido
+    }
 }
 function calcularGratificacion (sueldoBruto) {
     iMM =  450000     // Ingreso minimo mensual
